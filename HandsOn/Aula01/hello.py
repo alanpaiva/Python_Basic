6ff0706773135c2a108a0f2c9b244b1909c82b97
Alexander Nunes <alexandercn@hotmail.com>
2016-08-01 20:13:00 -0300

    Enviando arqruivos

---
+++ HandsOn/Aula01/hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+print "Hello World"
+
+#Para fazer o commit
+#Configurar usuario
+#git config --global user.name "Alexander Nunes"
+#git config --global user.email "alexandercn@hotmail.com"
+
+#git add --all
+#git add arquivo
+
+#commit do arquivo em reposit[orio local
+#git commit -m  "Enviando arqruivos"
+
+#Commit no repositorio remoto
+#git push
+
+
